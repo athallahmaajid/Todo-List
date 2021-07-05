9a40c3e5fbcd68c366bdfa5a376322b6abf61003
athallahmaajid <athallahmaajid@gmail.com>
2021-07-05 14:00:44 +0700

    add 404 page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from website import create_app
+from flask import render_template
 
 app = create_app()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
